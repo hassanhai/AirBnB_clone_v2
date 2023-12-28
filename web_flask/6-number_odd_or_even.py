cd45dc82e05b60d40d774155966e7ada6c849b8d
hassanhai <hassan.mouilha@gmail.com>
2023-12-28 01:28:40 -0800

    up

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,55 +1,62 @@
-#!/usrr/bin/python3
-""" Script that starts a Flask web application """
-from flask import Flask, render_template
+#!/usr/bin/python3
+"""Start web application with two routings
+"""
 
+from flask import Flask, render_template
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def hello_hbnb():
-    """ Print Web """
+def hello():
+    """Return string when route queried
+    """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
 def hbnb():
-    """ Print Web """
+    """Return string when route queried
+    """
     return 'HBNB'
 
 
 @app.route('/c/<text>')
 def c_is_fun(text):
-    """ Print a char C followed by the value of the text variable """
-    return 'C {}'.format(text.replace('_', ' '))
+    """Return reformatted text
+    """
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python')
+@app.route('/python/')
 @app.route('/python/<text>')
-def python_is_cool(text='is cool'):
-    """ Print Python, followed by the value of the text variable,
-    with default value of text: is cool """
-    return 'Python {}'.format(text.replace('_', ' '))
+def python_with_text(text='is cool'):
+    """Reformat text based on optional variable
+    """
+    return 'Python ' + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>')
-def number(n):
-    """ Number route """
-    return '{} is number'.format(n)
+def number(n=None):
+    """Allow request if path variable is a valid integer
+    """
+    return str(n) + ' is a number'
 
 
 @app.route('/number_template/<int:n>')
 def number_template(n):
-    """ Display a HTML page only if n is an integer """
-    return render_template('5-number.html', number=n)
+    """Retrieve template for request
+    """
+    path = '5-number.html'
+    return render_template(path, n=n)
 
 
 @app.route('/number_odd_or_even/<int:n>')
 def number_odd_or_even(n):
-    """ Show if the number is even or odd """
-    return render_template('6-number_odd_or_even.html', n=n)
-
+    """Render template based on conditional
+    """
+    path = '6-number_odd_or_even.html'
+    return render_template(path, n=n)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # app.run(host='0.0.0.0', port=5000)
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
