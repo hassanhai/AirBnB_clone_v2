f54749dcf8f4b7124b89d9bfee8e27e064465f11
hassanhai <hassan.mouilha@gmail.com>
2023-12-28 13:33:49 -0800

    up

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,31 +1,33 @@
 #!/usr/bin/python3
-"""Start web application with two routings
 """
-
+Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+/states_list: HTML page with a list of all State objects in DBStorage.
+"""
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def close_db(exc):
+    """close the current session of sqlalchemist"""
+    storage.close()
 
 
 @app.route('/states_list')
 def states_list():
-    """Render template with states
     """
-    path = '7-states_list.html'
-    states = storage.all(State)
-    # sort State object alphabetically by name
-    sorted_states = sorted(states.values(), key=lambda state: state.name)
-    return render_template(path, sorted_states=sorted_states)
-
-
-@app.teardown_appcontext
-def app_teardown(arg=None):
-    """Clean-up session
+    Displays an HTML page with a list of all State objects in DBStorage.
+    States are sorted by name.
     """
-    storage.close()
+    states = storage.all(State).values()
+    return render_template("7-states_list.html", states=states)
 
 
-if __name__ == '__main__':
-    app.url_map.strict_slashes = False
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
